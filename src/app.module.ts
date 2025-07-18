21ae242cfc610ee5ec3f5ca720f6e210e0967384
shivani <shivani.rana@appinventiv.com>
2025-07-18 16:09:03 +0530

    directory structure created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { AdminModule } from './admin/admin.module';
+import { UserModule } from './user/user.module';
+import { ServicesModule } from './services/services.module';
+import { UsersModule } from './users/users.module';
+import { PaymentModule } from './payment/payment.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, AdminModule, UserModule, ServicesModule, UsersModule, PaymentModule, OrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
